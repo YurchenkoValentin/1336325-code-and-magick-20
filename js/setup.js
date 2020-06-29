0c7f8bedb3deb2002e870d03c473e818ca356042
Валентин <60097296+YurchenkoValentin@users.noreply.github.com>
2020-06-29 16:59:40 +0300

    Добавляет выбор случайного мага

---
+++ js/setup.js
@@ -0,0 +1,52 @@
+'use strict';
+var WIZARD_COUNT = 4;
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var wizards = [];
+
+for (var i = 0; i < WIZARD_COUNT; i++) {
+  var randomNameTwo = names[Math.floor(Math.random() * names.length)];
+  var randomSurnameTwo = lastNames[Math.floor(Math.random() * lastNames.length)];
+  var randomCoat = coatColor[Math.floor(Math.random() * coatColor.length)];
+  var randomEyes = eyesColor[Math.floor(Math.random() * eyesColor.length)];
+
+  var name = randomNameTwo + ' ' + randomSurnameTwo;
+
+  var wizard = {
+    name: name,
+    coatColor: randomCoat,
+    eyesColor: randomEyes
+  };
+
+  wizards.push(wizard);
+}
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
